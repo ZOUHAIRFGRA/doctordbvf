3ded991eb9ebfb6cc3a07b48ae4090c725e343f8
Zouhair Fouiguira <88293311+ZOUHAIRFGRA@users.noreply.github.com>
2023-04-30 12:00:00 +0100

    fixing

--- graphes/graph_sp_n.php
+++ graphes/graph_sp_n.php
@@ -24,7 +24,7 @@ $nb7 = $row4[6]['nbr_pat'] ?? 0;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-    <title>Document</title>
+    <title>graph_sp_n</title>
 </head>
 
 <body>
