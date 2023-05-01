4eb7c83e43db53050416a146f60c176aff2a4fbc
Zouhair Fouiguira <88293311+ZOUHAIRFGRA@users.noreply.github.com>
2023-05-01 12:00:00 +0100

    fixing

--- graphes/graph_h_f.php
+++ graphes/graph_h_f.php
@@ -18,7 +18,7 @@ $nb2 = $row[0]['nombre_par_sexe'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.0.2/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
-    <title>Document</title>
+    <title>graphe h f</title>
 </head>
 
 <body>
